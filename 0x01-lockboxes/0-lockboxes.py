afbd75c91498ba44c7f5cdd240d6e84a48e8aa21
Olamide Yakub Lateef <98460197+Poltergeist1717@users.noreply.github.com>
2023-10-06 00:10:36 +0100

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
-
 from collections import deque
 
 def canUnlockAll(boxes):
+    """
+    Determine if all boxes can be opened.
+
+    Args:
+        boxes (List[List[int]]): A list of lists where each inner list represents a box and contains keys as positive integers.
+
+    Returns:
+        bool: True if all boxes can be opened, False otherwise.
+    """
     n = len(boxes)  # Total number of boxes
     visited = [False] * n  # Initialize a list to keep track of visited boxes
     visited[0] = True  # Mark the first box as visited
@@ -20,3 +28,6 @@ def canUnlockAll(boxes):
 
     return all(visited)  # Return True if all boxes are visited, else return False
 
+# Example usage:
+# boxes = [[1], [2], [3], []]
+# print(canUnlockAll(boxes))
